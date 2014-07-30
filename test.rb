5e0f62fa22bafd5f3ced10f2d861c921e698ead0
Michael Bottjer <michaelbottjer@hotmail.com>
2014-07-30 12:02:33 +0100

    convert array to hash

--- test.rb
+++ test.rb
@@ -4,11 +4,11 @@ require 'open-uri'
 
 url = "https://developer.mozilla.org/en/docs/Web/Guide/HTML/HTML5/HTML5_element_list"
 doc = Nokogiri::HTML(open(url))
-categories = ["#Interactive_elements" , "#Forms" , "#Tabular_data" , "#Embedded_content" , "#Edits" , "#Text-level_semantics" , "#Grouping_content" , "#Sections" , "#Scripting" , "#Document_metadata" , "#Root_element"]
+categories = {"#Root_element" => 1, "#Document_metadata" => 2, "#Scripting" => 3, "#Sections" => 4, "#Grouping_content" => 5, "#Text-level_semantics" => 6, "#Edits" => 7, "#Embedded_content" => 8, "#Tabular_data" => 9, "#Forms" => 10, "#Interactive_elements" => 11}
 
-categories.each do |category|
+categories.each_key do |category|
 	doc.css(category + "+ .standard-table td").each do |item|
-		puts category
+		puts categories[category]
 		puts item.css(":nth-child(1)").text[/\<(.*?)>/,1]
 		puts item.css("+ td").text.gsub(/\s+/, " ")
 	end
