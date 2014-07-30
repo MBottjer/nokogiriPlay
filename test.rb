6e5da683be08a1f64705f165cd2ccd5078932df0
Michael Bottjer <michaelbottjer@hotmail.com>
2014-07-30 11:54:15 +0100

    add category selection

--- test.rb
+++ test.rb
@@ -4,7 +4,12 @@ require 'open-uri'
 
 url = "https://developer.mozilla.org/en/docs/Web/Guide/HTML/HTML5/HTML5_element_list"
 doc = Nokogiri::HTML(open(url))
-doc.css("#wikiArticle td").each do |item|
-	puts item.css(":nth-child(1)").text[/\<(.*?)>/,1]
-	puts item.css("+ td").text.gsub(/\s+/, " ")
+categories = ["#Interactive_elements" , "#Forms" , "#Tabular_data" , "#Embedded_content" , "#Edits" , "#Text-level_semantics" , "#Grouping_content" , "#Sections" , "#Scripting" , "#Document_metadata" , "#Root_element"]
+
+categories.each do |category|
+	doc.css(category + "+ .standard-table td").each do |item|
+		puts category
+		puts item.css(":nth-child(1)").text[/\<(.*?)>/,1]
+		puts item.css("+ td").text.gsub(/\s+/, " ")
+	end
 end
